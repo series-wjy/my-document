4b81761b5ce6fbc2af12bc34d540583cfa18c3de
wangjiayou <seenrabbit@gmail.com>
2019-08-12 22:10:05 +0800

    提交

---
+++ springcloud/spring-cloud-base/microservice-simple/src/main/java/com/wjy/cloud/MicroserviceSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.wjy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleApplication.class, args);
+	}
+
+}
