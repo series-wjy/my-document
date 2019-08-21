6d35907c99da62a2ffafe1d514f557637dfb4727
wangjiayou <wangjiayou@travelsky.com>
2019-08-21 14:50:04 +0800

    提交

---
+++ kafka/kafka-base/kafka-consumer/src/main/java/com/ts/consumer/SyncManualConsumer.java
@@ -0,0 +1,50 @@
+package com.ts.consumer;
+
+import kafka.utils.ShutdownableThread;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class SyncManualConsumer extends ShutdownableThread {
+    private KafkaConsumer<Integer, String> consumer;
+
+    public SyncManualConsumer() {
+        super("KafkaConsumerTest", false);
+
+        Properties properties = new Properties();
+        String brokers = "kafkaOS1:9092,kafkaOS2:9092,kafkaOS3:9092";
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "cityGro11");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        // properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        // 设置一次提交的offset个数
+        properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.IntegerDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+
+        this.consumer = new KafkaConsumer<Integer, String>(properties);
+    }
+
+    @Override
+    public void doWork() {
+        // 指定要消费的主题
+        consumer.subscribe(Collections.singletonList("cities"));
+        ConsumerRecords<Integer, String> records = consumer.poll(1000);
+        for(ConsumerRecord record : records) {
+            System.out.print("topic = " + record.topic());
+            System.out.print(" partition = " + record.partition());
+            System.out.print(" key = " + record.key());
+            System.out.println(" value = " + record.value());
+
+            // 手动同步提交
+            consumer.commitSync();
+        }
+    }
+}
