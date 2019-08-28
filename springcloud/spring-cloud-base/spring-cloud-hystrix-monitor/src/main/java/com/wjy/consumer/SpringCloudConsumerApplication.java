662d05dc197f3b2fe28fb8a7b7d13c3a4e4dec2a
wangjiayou <wangjiayou@travelsky.com>
2019-08-28 17:25:14 +0800

    提交

---
+++ springcloud/spring-cloud-base/spring-cloud-hystrix-monitor/src/main/java/com/wjy/consumer/SpringCloudConsumerApplication.java
@@ -0,0 +1,22 @@
+package com.wjy.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class SpringCloudConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConsumerApplication.class, args);
+	}
+
+}
---
+++ springcloud/spring-cloud-base/spring-cloud-provider/src/main/java/com/wjy/provider/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.wjy.provider.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        System.out.println(name + "comming here......");
+        return "hello "+name+"，this is first messge";
+    }
+}
