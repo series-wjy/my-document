3191d9da208811bfee00d1739b602abcdf505f8a
wangjiayou <seenrabbit@gmail.com>
2019-08-28 23:33:46 +0800

    提交

--- springcloud/spring-cloud-base/spring-cloud-hystrix-monitor/src/main/java/com/wjy/consumer/SpringCloudConsumerApplication.java
+++ springcloud/spring-cloud-base/spring-cloud-hystrix-monitor/src/main/java/com/wjy/consumer/SpringCloudConsumerApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrixDashboard
